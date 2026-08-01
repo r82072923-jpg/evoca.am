307f48693c4380d2b492d167d302b447ac58b201
Rubik <r82072923@gmail.com>
2026-08-01 14:55:35 +0400

    Added evoca-imasin2 in the mermasin page

--- src/components/mermasin.jsx
+++ src/components/mermasin.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader3 from "./topHeader3"
 import EvocaiMasin from "./evoca-imasin"
+import EvocaiMasin2 from "./evoca-imasin2"
 function MerMasin(){
     return(
         <>
         <Header></Header>
         <TopHeader3></TopHeader3>
         <EvocaiMasin></EvocaiMasin>
+        <EvocaiMasin2></EvocaiMasin2>
         </>
     )
 }
