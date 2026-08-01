22b1bbbb06d2c9837abab6b695acb581f22e6673
Rubik <r82072923@gmail.com>
2026-08-01 11:52:28 +0400

    Added footerBottom in akntartayinvjarumer page

--- src/components/akntartayinvjarumner.jsx
+++ src/components/akntartayinvjarumner.jsx
@@ -1,12 +1,14 @@
 import Header2 from "./header2"
 import Vjarumner from "./vjarumner"
 import Footer2 from "./footer2"
+import FooterBottom from "./footerBottom"
 function AkntartayinVjarumner(){
     return(
         <>
         <Header2></Header2>
         <Vjarumner></Vjarumner>
         <Footer2></Footer2>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
