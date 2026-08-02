b1cad88216b8337a46b53b39689b61da9c5b16a8
Rubik <r82072923@gmail.com>
2026-08-02 12:53:27 +0400

    Added news3 in the news page

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -3,6 +3,7 @@ import TopHeader4 from "./topHeader4"
 import NewsiBajinner from "./news-ibajinner"
 import News1 from "./news1"
 import News2 from "./news2"
+import News3 from "./news3"
 function News(){
     return(
         <>
@@ -11,6 +12,7 @@ function News(){
             <NewsiBajinner></NewsiBajinner>
             <News1></News1>
             <News2></News2>
+            <News3></News3>
         </>
     )
 }
