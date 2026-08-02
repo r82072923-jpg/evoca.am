8230d40e61571626d892894bcec755861af5c8ad
Rubik <r82072923@gmail.com>
2026-08-02 11:30:32 +0400

    Added newsibajinner in the news page

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import NewsiBajinner from "./news-ibajinner"
 function News(){
     return(
         <>
             <Header></Header>
             <TopHeader4></TopHeader4>
+            <NewsiBajinner></NewsiBajinner>
         </>
     )
 }
