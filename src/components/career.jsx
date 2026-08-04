604c07db62e9bf66b5b822bc39423cd3838ea7f0
Rubik <r82072923@gmail.com>
2026-08-04 14:53:39 +0400

    Added slayder3 in the career page

--- src/components/career.jsx
+++ src/components/career.jsx
@@ -2,13 +2,15 @@ import Header from "./header"
 import TopHeader5 from "./topHeader5"
 import Careers1 from "./careers1"
 import Careers2 from "./careers2"
+import Slayder3 from "./slayder3"
 function Career(){
     return(
         <>
         <Header></Header> 
         <TopHeader5></TopHeader5>
         <Careers1></Careers1> 
-        <Careers2></Careers2>   
+        <Careers2></Careers2>  
+        <Slayder3></Slayder3> 
         </>
     )
 }
