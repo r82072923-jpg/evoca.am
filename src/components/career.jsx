4a8a5c494f2f7d7354f926c04465db6b24c61041
Rubik <r82072923@gmail.com>
2026-08-04 14:15:47 +0400

    Added careers1 in the career page

--- src/components/career.jsx
+++ src/components/career.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader5 from "./topHeader5"
+import Careers1 from "./careers1"
 function Career(){
     return(
         <>
         <Header></Header> 
-        <TopHeader5></TopHeader5>    
+        <TopHeader5></TopHeader5>
+        <Careers1></Careers1>    
         </>
     )
 }
