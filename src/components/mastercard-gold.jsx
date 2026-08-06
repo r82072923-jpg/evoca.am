7bd26865bddc0dbdaceaf0a363707b6780beb630
Rubik <r82072923@gmail.com>
2026-08-06 14:32:06 +0400

    Added footerBottom in the mastercatrd-gold page

--- src/components/mastercard-gold.jsx
+++ src/components/mastercard-gold.jsx
@@ -7,6 +7,7 @@ import MasterCardGoldiMasin3 from "./mastercard-gold-imasin3"
 import Slayder5 from "./slayder5"
 import OnlineEvMobileBanking from "./onlineevmobilebanking"
 import Footer from "./footer"
+import FooterBottom from "./footerBottom"
 function MasterCardGold(){
     const [activeTab, setActiveTab] = useState('Քարտի մասին');
     return(
@@ -26,6 +27,7 @@ function MasterCardGold(){
         <Slayder5></Slayder5>
         <OnlineEvMobileBanking></OnlineEvMobileBanking>
         <Footer></Footer>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
