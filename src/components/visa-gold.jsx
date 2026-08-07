7bad09996a228e0502115d7f68667ace7dd7ec5a
Rubik <r82072923@gmail.com>
2026-08-07 12:45:32 +0400

    Added footer in the visa-gold page

--- src/components/visa-gold.jsx
+++ src/components/visa-gold.jsx
@@ -7,6 +7,7 @@ import VisaGoldiMasin3 from "./visa-gold-imasin3"
 import VisaGoldiMasin4 from "./visa-gold-imasin4"
 import Slayder5 from "./slayder5"
 import OnlineEvMobileBanking from "./onlineevmobilebanking"
+import Footer from "./footer"
 function VisaGold(){
     const [activeTab, setActiveTab] = useState('Քարտի մասին');
     return(
@@ -26,6 +27,7 @@ function VisaGold(){
         <VisaGoldiMasin4></VisaGoldiMasin4>
         <Slayder5></Slayder5>
         <OnlineEvMobileBanking></OnlineEvMobileBanking>
+        <Footer></Footer>
         </>
     )
 }
