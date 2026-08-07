9ba1aa7b22a3a67c6639634f3a11a7a4c67addc7
Rubik <r82072923@gmail.com>
2026-08-07 15:16:28 +0400

    Added footerBottom in the travelgold page

--- src/components/travelgold.jsx
+++ src/components/travelgold.jsx
@@ -7,6 +7,7 @@ import TravelGoldiMasin3 from "./travelgold-imasin3"
 import Slayder5 from "./slayder5";
 import OnlineEvMobileBanking from "./onlineevmobilebanking";
 import Footer from "./footer";
+import FooterBottom from "./footerBottom";
 function TravelGold(){
     const [activeTab, setActiveTab] = useState('Քարտի մասին');
     return(
@@ -26,6 +27,7 @@ function TravelGold(){
         <Slayder5></Slayder5>
         <OnlineEvMobileBanking></OnlineEvMobileBanking>
         <Footer></Footer>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
