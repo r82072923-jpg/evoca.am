2d30f0fbaac1507bc13b5bcd30f65c80cdd26e06
Rubik <r82072923@gmail.com>
2026-08-08 10:52:24 +0400

    Added visavision-imasin2 in the visavision page

--- src/components/visavision.jsx
+++ src/components/visavision.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import VisaVisioniMasin from "./visavision-imasin"
+import VisaVisioniMasin2 from "./visavision-imasin2"
 function VisaVision(){
+    const [activeTab, setActiveTab] = useState('Քարտի մասին');
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <VisaVisioniMasin></VisaVisioniMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <VisaVisioniMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Visa Vision քարտի սակագներ' && (
+            <VisaInfiniteCardiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
