50efc5cd4426c7db37ebfd8b4e48633bcbde93b8
Rubik <r82072923@gmail.com>
2026-08-08 12:08:50 +0400

    Added unionpay-gold-imasin2 in the unionpay-gold page

--- src/components/unionpay-gold.jsx
+++ src/components/unionpay-gold.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import UnionPayGoldiMasin from "./unionpay-gold-imasin"
+import UnionPayGoldiMasin2 from "./unionpay-gold-imasin2"
 function UnionPayGold(){
+    const [activeTab, setActiveTab] = useState('Քարտի մասին');
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <UnionPayGoldiMasin></UnionPayGoldiMasin>
+        <div>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <UnionPayGoldiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սակագներ և դրույթներ' && (
+            <VisaGoldiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
+        </div>
         </>
     )
 }
