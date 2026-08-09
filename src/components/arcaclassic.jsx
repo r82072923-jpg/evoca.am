0337f734417c22829d7db73d9528b8240b56226b
Rubik <r82072923@gmail.com>
2026-08-09 23:15:34 +0400

    Added arcaclassic-imasin2 in the arcaclassic page

--- src/components/arcaclassic.jsx
+++ src/components/arcaclassic.jsx
@@ -1,12 +1,24 @@
 import Header from "./header"
 import TopHeader from "./topHeader"
 import ArcaClassiciMasin from "./arcaclassic-imasin"
+import ArcaClassiciMasin2 from "./arcaclassic-imasin2"
+import { useState } from "react"
 function ArcaClassic(){
+    const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <ArcaClassiciMasin></ArcaClassiciMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <ArcaClassiciMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սակագներ և դրույթներ' && (
+            <FourUamGiftCardiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
