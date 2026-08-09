8687c2d23ce510c8b1a069ccfc3389c27a52683a
Rubik <r82072923@gmail.com>
2026-08-09 22:03:32 +0400

    Added slayder5 in the 4u.am-giftcard page

--- src/components/4u.am-giftcard.jsx
+++ src/components/4u.am-giftcard.jsx
@@ -5,6 +5,7 @@ import FourUamGiftCardiMasin from "./4u.am-giftcard-imasin"
 import FourUamGiftCardiMasin2 from "./4u.am-giftcard-imasin2"
 import FourUamGiftCardiMasin3 from "./4u.am-giftcard-imasin3"
 import FourUamGiftCardiMasin4 from "./4u.am-giftcard-imasin4"
+import Slayder5 from "./slayder5"
 function FourUamGiftCard(){
         const [activeTab, setActiveTab] = useState('Քարտի մասին');
     return(
@@ -25,6 +26,7 @@ function FourUamGiftCard(){
             <FourUamGiftCardiMasin4 activeTab={activeTab} setActiveTab={setActiveTab} />
           )}
         </div>
+        <Slayder5></Slayder5>
         </>
     )
 }
