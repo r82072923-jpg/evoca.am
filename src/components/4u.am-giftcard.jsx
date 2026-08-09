988c3d9598242d1c6d7f9efac8792eccc5de5829
Rubik <r82072923@gmail.com>
2026-08-09 16:31:18 +0400

    Added 4u.am-giftcard-imasin2 in the 4u.amgidtcard page

--- src/components/4u.am-giftcard.jsx
+++ src/components/4u.am-giftcard.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import Header from "./header"
 import TopHeader from "./topHeader"
 import FourUamGiftCardiMasin from "./4u.am-giftcard-imasin"
+import FourUamGiftCardiMasin2 from "./4u.am-giftcard-imasin2"
 function FourUamGiftCard(){
+        const [activeTab, setActiveTab] = useState('Քարտի մասին');
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <FourUamGiftCardiMasin></FourUamGiftCardiMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <FourUamGiftCardiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Տրամադրման պայմանները' && (
+            <MasterCardGoldiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սպասարկման պայմանները' && (
+            <MasterCardGoldiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
