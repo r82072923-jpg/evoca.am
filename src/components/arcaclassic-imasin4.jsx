47934f335fb9e505739e9d622cc1b8bd1b7fe2ba
Rubik <r82072923@gmail.com>
2026-08-09 23:56:23 +0400

    Added arcaclassic-imasin4 in the arcaclassic page

---
+++ src/components/arcaclassic-imasin4.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+function ArcaClassiciMasin4() {
+  return (
+    <div className="relative w-full h-[350px] sm:h-[400px] bg-[#3a007d] flex items-center justify-center overflow-hidden font-sans">
+      
+      <img
+        src="https://www.evoca.am/images-cache/cards/1/16131316712977/1920x527.jpg"
+        alt="Evocabank background"
+        className="absolute inset-0 w-full h-full object-cover z-0 opacity-90"
+      />
+
+
+      {/* Կենտրոնական տեքստ */}
+      <div className="relative z-10 text-center px-6 max-w-4xl mx-auto">
+        <h1 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold leading-snug sm:leading-relaxed drop-shadow-md">
+          Արագ և հուսալի գնումներ ու վճարումներ <br className="hidden md:block" />
+          ՀՀ ամբողջ տարածքում` քո EvocaTOUCH <br className="hidden md:block" />
+          վճարային քարտով:
+        </h1>
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default ArcaClassiciMasin4;
--- src/components/arcaclassic.jsx
+++ src/components/arcaclassic.jsx
@@ -3,6 +3,7 @@ import TopHeader from "./topHeader"
 import ArcaClassiciMasin from "./arcaclassic-imasin"
 import ArcaClassiciMasin2 from "./arcaclassic-imasin2"
 import ArcaClassiciMasin3 from "./arcaclassic-imasin3"
+import ArcaClassiciMasin4 from "./arcaclassic-imasin4"
 import { useState } from "react"
 function ArcaClassic(){
     const [activeTab,setActiveTab]=useState("Քարտի մասին")
@@ -20,6 +21,7 @@ function ArcaClassic(){
             <ArcaClassiciMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
           )}
         </div>
+        <ArcaClassiciMasin4></ArcaClassiciMasin4>
         </>
     )
 }
