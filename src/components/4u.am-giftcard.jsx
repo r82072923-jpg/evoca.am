970044f6029c942a7d57272075d704e221aca1fa
Rubik <r82072923@gmail.com>
2026-08-09 22:04:37 +0400

    Added onlineEvMobileBanking in the 4u.am-giftcard page

--- src/components/4u.am-giftcard.jsx
+++ src/components/4u.am-giftcard.jsx
@@ -6,6 +6,7 @@ import FourUamGiftCardiMasin2 from "./4u.am-giftcard-imasin2"
 import FourUamGiftCardiMasin3 from "./4u.am-giftcard-imasin3"
 import FourUamGiftCardiMasin4 from "./4u.am-giftcard-imasin4"
 import Slayder5 from "./slayder5"
+import OnlineEvMobileBanking from "./onlineevmobilebanking";
 function FourUamGiftCard(){
         const [activeTab, setActiveTab] = useState('Քարտի մասին');
     return(
@@ -27,6 +28,7 @@ function FourUamGiftCard(){
           )}
         </div>
         <Slayder5></Slayder5>
+        <OnlineEvMobileBanking></OnlineEvMobileBanking>
         </>
     )
 }
