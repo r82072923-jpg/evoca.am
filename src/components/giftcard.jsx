48565e98eb1a101a4d3489f5042ef247008589f9
Rubik <r82072923@gmail.com>
2026-08-09 22:46:04 +0400

    Added giftcard-imasin2 in the giftcard page

--- src/components/giftcard.jsx
+++ src/components/giftcard.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import GiftCardiMasin from "./giftcard-imasin"
+import GiftCardiMasin2 from "./giftcard-imasin2"
 function GiftCard(){
+    const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <GiftCardiMasin></GiftCardiMasin>
+        <div>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <GiftCardiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Տրամադրման պայմանները' && (
+            <FourUamGiftCardiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սպասարկման պայմանները' && (            
+            <FourUamGiftCardiMasin4 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
+        </div>
         </>
     )
 }
