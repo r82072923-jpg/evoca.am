ac7cfb2ccc4a32b005f265dae83222ea423cc15f
Rubik <r82072923@gmail.com>
2026-08-09 15:22:17 +0400

    Added visaplatinum-imasin2 in the visaplatinum page

--- src/components/visaplatinum.jsx
+++ src/components/visaplatinum.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import VisaPlatinumiMasin from "./visaplatinum-imasin"
+import VisaPlatinumiMasin2 from "./visaplatinum-imasin2"
 function VisaPlatinum(){
+    const [activeTab, setActiveTab] = useState('Քարտի մասին');
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <VisaPlatinumiMasin></VisaPlatinumiMasin>
+        <div>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <VisaPlatinumiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սակագներ և դրույթներ' && (
+            <VisaInfiniteCardiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
+        </div>
         </>
     )
 }
