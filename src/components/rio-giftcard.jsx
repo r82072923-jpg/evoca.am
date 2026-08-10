24b65ac370509d5c3c3c18cb8cef3f6be926f970
Rubik <r82072923@gmail.com>
2026-08-10 14:43:44 +0400

    Added rio-giftcard-imasin2 in the rio-giftcard page

--- src/components/rio-giftcard.jsx
+++ src/components/rio-giftcard.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import RioGiftCardiMasin from "./rio-giftcard-imasin"
+import RioGiftCardiMasin2 from "./rio-giftcard-masin2"
 function RioGiftCard(){
+    const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <RioGiftCardiMasin></RioGiftCardiMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <RioGiftCardiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Տրամադրման պայմանները' && (
+            <DalmaGiftCardiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սպասարկման պայմանները' && (            
+            <FourUamGiftCardiMasin4 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
