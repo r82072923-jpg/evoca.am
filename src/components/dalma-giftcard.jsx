a34e83193c699d6cab679c3067d98c4f4eda4146
Rubik <r82072923@gmail.com>
2026-08-10 13:36:28 +0400

    Added dalma-giftcard-imasin2 in the dalma-giftcard page

--- src/components/dalma-giftcard.jsx
+++ src/components/dalma-giftcard.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import DalmaGiftCardiMasin from "./dalma-giftcard-imasin"
+import DalmaGiftCardiMasin2 from "./dalma-giftcard-imasin2"
 function DalmaGiftCard(){
+    const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <DalmaGiftCardiMasin></DalmaGiftCardiMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <DalmaGiftCardiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Տրամադրման պայմանները' && (
+            <FourUamGiftCardiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սպասարկման պայմանները' && (            
+            <FourUamGiftCardiMasin4 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
