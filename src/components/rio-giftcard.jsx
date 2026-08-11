4097461ffcd0bfc2e7b6acaa1d05e5809bf01ee4
Rubik <r82072923@gmail.com>
2026-08-11 11:10:16 +0400

    Added onlineEvMobileBanking in the rio-giftcard page

--- src/components/rio-giftcard.jsx
+++ src/components/rio-giftcard.jsx
@@ -6,6 +6,7 @@ import RioGiftCardiMasin2 from "./rio-giftcard-masin2"
 import RioGiftCardiMasin3 from "./rio-giftcard-imasin3"
 import FourUamGiftCardiMasin4 from "./4u.am-giftcard-imasin4"
 import Slayder5 from "./slayder5"
+import OnlineEvMobileBanking from"./onlineevmobilebanking"
 function RioGiftCard(){
     const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
@@ -27,6 +28,7 @@ function RioGiftCard(){
           )}
         </div>
         <Slayder5></Slayder5>
+        <OnlineEvMobileBanking></OnlineEvMobileBanking>
         </>
     )
 }
