a00c2f6f85ef4c5ec0c57ad14def4417694f3692
Rubik <r82072923@gmail.com>
2026-08-11 12:12:26 +0400

    Added onlineEvMobileBanking in the world-digtal page

--- src/components/world-digital.jsx
+++ src/components/world-digital.jsx
@@ -5,6 +5,7 @@ import WorldDigitaliMasin from "./world-digital-imasin"
 import WorldDigitaliMasin2 from "./world-digital-imasin2"
 import WorldDigitaliMasin3 from "./world-digital-imasin3"
 import Slayder5 from "./slayder5"
+import OnlineEvMobileBanking from "./onlineevmobilebanking"
 function WorldDigital(){
     const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
@@ -22,6 +23,7 @@ function WorldDigital(){
           )}
         </div>
         <Slayder5></Slayder5>
+        <OnlineEvMobileBanking></OnlineEvMobileBanking>
         </>
     )
 }
