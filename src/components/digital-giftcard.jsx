155c5bf2dfac0672e246e2df701eeb9f00c8aff6
Rubik <r82072923@gmail.com>
2026-08-11 12:43:52 +0400

    Addeds digital-giftcard-imasin2 in the digital-giftcard page

--- src/components/digital-giftcard.jsx
+++ src/components/digital-giftcard.jsx
@@ -1,12 +1,24 @@
 import Header from "./header"
 import TopHeader from "./topHeader"
 import DigitalGiftCardiMasin from "./digital-giftcard-imasin"
+import DigitalGiftCardiMasin2 from "./digital-giftcard-imasin2"
+import { useState } from "react"
 function DigitalGiftCard(){
+const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <DigitalGiftCardiMasin></DigitalGiftCardiMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <DigitalGiftCardiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սակագներ և դրույթներ' && (
+            <WorldDigitaliMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
