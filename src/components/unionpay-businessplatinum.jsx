ca62b3608437e851c20c87f856265bba9297a280
Rubik <r82072923@gmail.com>
2026-08-11 13:37:43 +0400

    Added unionpay-businessplatinum-imasin2 in the unionpay-businessplatinum page

--- src/components/unionpay-businessplatinum.jsx
+++ src/components/unionpay-businessplatinum.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import UnionPayBusinessPlatinumiMasin from "./unionpay-businessplatinum-imasin"
+import UnionPayBusinessPlatinumiMasin2 from "./unionpay-businessplatinum-imasin2"
 function UnionPayBusinessPlatinum(){
+    const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <UnionPayBusinessPlatinumiMasin></UnionPayBusinessPlatinumiMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <UnionPayBusinessPlatinumiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սակագներ և դրույթներ' && (
+            <DigitalGiftCardiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
