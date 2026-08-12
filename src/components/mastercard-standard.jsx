6c774209fac33874cf8d4cdd0cd7b67902f2734c
Rubik <r82072923@gmail.com>
2026-08-12 17:58:17 +0400

    Added mastercard-standard-imasin2 in the mastercard-standard page

--- src/components/mastercard-standard.jsx
+++ src/components/mastercard-standard.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import MasterCardStandardiMasin from "./mastercard-standard-imasin"
+import MasterCardStandardiMasin2 from "./mastercard-standard-imasin2"
 function MasterCardStandard(){
+    const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <MasterCardStandardiMasin></MasterCardStandardiMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <MasterCardStandardiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սակագներ և դրույթներ' && (
+            <VisaGoldiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
