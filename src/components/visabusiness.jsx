7fcf5095f70c0311699e5c15e77a8d8f9c018cdc
Rubik <r82072923@gmail.com>
2026-08-12 12:09:19 +0400

    Added visabusiness-imasin2 in the visabusiness page

--- src/components/visabusiness.jsx
+++ src/components/visabusiness.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import VisaBusinessiMasin from "./visabusiness-imasin"
+import VisaBusinessiMasin2 from "./visabusiness-imasin2"
 function VisaBusiness(){
+    const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
         <>
         <Header></Header>
         <TopHeader> </TopHeader>
         <VisaBusinessiMasin></VisaBusinessiMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <VisaBusinessiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սակագներ և դրույթներ' && (
+            <WilcoVisaInfiniteiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          {activeTab === 'Օգտակար խորհուդներ' && (
+            <WilcoVisaInfiniteiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          {activeTab === 'Զգուշացում' && (
+            <WilcoVisaInfiniteiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
