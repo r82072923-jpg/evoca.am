bfe870f61a1dcb59103f125c45dc9daaf732a089
Rubik <r82072923@gmail.com>
2026-08-12 11:22:01 +0400

    Added wilco-visainfinite-imasin2 in the wilco-visainfinite page

--- src/components/wilco-visainfinite.jsx
+++ src/components/wilco-visainfinite.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import WilcoVisaInfiniteiMasin from "./wilco-visainfinite-imasin"
+import WilcoVisaInfiniteiMasin2 from "./wilco-visainfinite-imasin2"
 function WilcoVisaInfinite(){
+    const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <WilcoVisaInfiniteiMasin></WilcoVisaInfiniteiMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <WilcoVisaInfiniteiMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սակագներ և դրույթներ' && (
+            <MyLeriMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
