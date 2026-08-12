eca12b90d50b916c48b5e9d11d45ff83dab9e31f
Rubik <r82072923@gmail.com>
2026-08-12 16:04:05 +0400

    Added visaclassic-masin2 in the visaclassic page

--- src/components/visaclassic.jsx
+++ src/components/visaclassic.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import VisaClassiciMasin from "./visaclassic-imasin"
+import VisaClassiciMasin2 from "./visaclassic-imasin2"
 function VisaClassic(){
+    const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <VisaClassiciMasin></VisaClassiciMasin>
+        <div>
+          {activeTab === 'Քարտի մասին' && (
+            <VisaClassiciMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սակագներ և դրույթներ' && (
+            <VisaDigitaliMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
