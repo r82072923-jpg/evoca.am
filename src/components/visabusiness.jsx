633a749ec6d8fcc7c0b2c9f13f5ed6a29f923e39
Rubik <r82072923@gmail.com>
2026-08-12 13:37:02 +0400

    Added onlineEvMobileBanking in the visabusiness page

--- src/components/visabusiness.jsx
+++ src/components/visabusiness.jsx
@@ -8,6 +8,7 @@ import VisaBusinessiMasin4 from "./visabusiness-imasin4"
 import VisaBusinessiMasin5 from "./visabusiness-imasin5"
 import VisaBusinessiMasin6 from "./visabusiness-imasin6"
 import Slayder5 from "./slayder5"
+import OnlineEvMobileBanking from "./onlineevmobilebanking"
 function VisaBusiness(){
     const [activeTab,setActiveTab]=useState("Քարտի մասին")
     return(
@@ -32,6 +33,7 @@ function VisaBusiness(){
         </div>
         <VisaBusinessiMasin6></VisaBusinessiMasin6>
         <Slayder5></Slayder5>
+        <OnlineEvMobileBanking></OnlineEvMobileBanking>
         </>
     )
 }
