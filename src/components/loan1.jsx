e832eedd009db582cbbd961e6f14d34263a3799f
Rubik <r82072923@gmail.com>
2026-08-15 19:54:46 +0400

    Added loan1-imasin2 in the loan1 page

--- src/components/loan1.jsx
+++ src/components/loan1.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import LoansiTopHeader from "./loans-itopHeader"
 import Loan1iMasin from "./loan1-imasin"
+import Loan1iMasin2 from "./loan1-imasin2"
 function Loan1(){
+    const [activeTab, setActiveTab] = useState('Վարկի մասին');
+
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <LoansiTopHeader></LoansiTopHeader>
         <Loan1iMasin></Loan1iMasin>
+        <div>
+          {activeTab === 'Վարկի մասին' && (
+            <Loan1iMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Տրամադրման պայմանները' && (
+            <FourUamGiftCardiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          
+          {activeTab === 'Սպասարկման պայմանները' && (            
+            <FourUamGiftCardiMasin4 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
