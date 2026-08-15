8c3eafd6e5b24ae4972400e86af064ae59a6bde9
Rubik <r82072923@gmail.com>
2026-08-15 13:00:47 +0400

    Added footer in the loans page

--- src/components/loans.jsx
+++ src/components/loans.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader from "./topHeader"
 import LoansiMasin from "./loans-imasin"
+import Footer from "./footer"
 function Loans(){
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <LoansiMasin></LoansiMasin>
+        <Footer></Footer>
         </>
     )
 }
