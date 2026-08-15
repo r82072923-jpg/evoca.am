c95a7e62e9de0f534f37f7294871fe30ac0f3f79
Rubik <r82072923@gmail.com>
2026-08-15 23:58:47 +0400

    Added loan2-imasin4 in the loan2 page

--- src/components/loan2.jsx
+++ src/components/loan2.jsx
@@ -5,6 +5,7 @@ import LoansiTopHeader from "./loans-itopHeader"
 import Loan2iMasin from "./loan2-imasin"
 import Loan2iMasin2 from "./loan2-imasin2"
 import Loan2iMasin3 from "./loan2-imasin3"
+import Loan2iMasin4 from "./loan2-imasin4"
 function Loan2(){
     const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
@@ -22,7 +23,7 @@ function Loan2(){
             <Loan2iMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
           )}
           {activeTab === 'Գործընկեր ավտոսրահներ' && (
-            <Loan1iMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+            <Loan2iMasin4 activeTab={activeTab} setActiveTab={setActiveTab} />
           )}
           {activeTab === 'Պահանջվող փաստաթղթերի ցանկ' && (            
             <Loan1iMasin4 activeTab={activeTab} setActiveTab={setActiveTab} />
