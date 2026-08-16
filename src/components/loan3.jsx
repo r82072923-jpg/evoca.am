94f70428b523428756cb244716aec632bd76c95e
Rubik <r82072923@gmail.com>
2026-08-16 15:51:30 +0400

    Added onlineEvMobileBanking in the oan3 page

--- src/components/loan3.jsx
+++ src/components/loan3.jsx
@@ -8,6 +8,7 @@ import Loan3iMasin3 from "./loan3-imasin3"
 import Loan3iMasin4 from "./loan3-imasin4"
 import Loan3iMasin5 from "./loan3-imasin5"
 import Slayder6 from "./slayder6"
+import OnlineEvMobileBanking from "./onlineevmobilebanking"
 function Loan3(){
     const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
@@ -30,6 +31,7 @@ function Loan3(){
         </div>
         <Loan3iMasin5></Loan3iMasin5>
         <Slayder6></Slayder6>
+        <OnlineEvMobileBanking></OnlineEvMobileBanking>
         </>
     )
 }
