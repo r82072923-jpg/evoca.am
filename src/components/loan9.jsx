3f74baf253042325b569fbc64d628cb5a8cdf25e
Rubik <r82072923@gmail.com>
2026-08-18 16:31:15 -0700

    Added loan9-imasin in the loan9 page

--- src/components/loan9.jsx
+++ src/components/loan9.jsx
@@ -1,14 +1,27 @@
 import Header from "./header"
 import TopHeader from "./topHeader"
 import LoansiTopHeader from "./loans-itopHeader"
-import Loan9iMasin from "./loan9-imaisn"
+import Loan9iMasin from "./loan9-imasin"
+import Loan9iMasin2 from "./loan9-imasin2"
 function Loan9(){
+    const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <LoansiTopHeader></LoansiTopHeader>
         <Loan9iMasin></Loan9iMasin>
+        <div>
+          {activeTab === 'Վարկի մասին' && (
+            <Loan8iMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          {activeTab === 'Պայմաններ' && (
+            <Loan8iMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          {activeTab === 'Պահանջվող փաստաթղթերի ցանկ' && (
+            <Loan8iMasin4 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
