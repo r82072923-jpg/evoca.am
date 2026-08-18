eda6f332c953a516746029b886826b2321c0f6ad
Rubik <r82072923@gmail.com>
2026-08-18 13:27:05 -0700

    Added loan8-imasin in the loan8 page

--- src/components/loan8.jsx
+++ src/components/loan8.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import LoansiTopHeader from "./loans-itopHeader"
-import Loan8iMasin from "./loan8-imaisn"
+import Loan8iMasin from "./loan8-imasin"
+import Loan8iMasin2 from "./loan8-imasin2"
 function Loan8(){
+    const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <LoansiTopHeader></LoansiTopHeader>
         <Loan8iMasin></Loan8iMasin>
+        <div>
+          {activeTab === 'Վարկի մասին' && (
+            <Loan8iMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          {activeTab === 'Պայմաններ' && (
+            <Loan7iMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          {activeTab === 'Պահանջվող փաստաթղթերի ցանկ' && (
+            <Loan7iMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
