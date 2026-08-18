f044a70a59161793be7c063ce8767704ce601254
Rubik <r82072923@gmail.com>
2026-08-18 16:20:28 -0700

    Added onlineEvMobileBanking in the loan8 page

--- src/components/loan8.jsx
+++ src/components/loan8.jsx
@@ -8,6 +8,7 @@ import Loan8iMasin3 from "./loan8-imasin3"
 import Loan8iMasin4 from "./loan8-imasin4"
 import Loan8iMasin5 from "./loan8-imasin5"
 import Slayder6 from "./slayder6"
+import OnlineEvMobileBanking from "./onlineevmobilebanking"
 function Loan8(){
     const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
@@ -29,6 +30,7 @@ function Loan8(){
         </div>
         <Loan8iMasin5></Loan8iMasin5>
         <Slayder6></Slayder6>
+        <OnlineEvMobileBanking></OnlineEvMobileBanking>
         </>
     )
 }
