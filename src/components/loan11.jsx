fec799262244c50df98addf358b7fd1f392e16b1
Rubik <r82072923@gmail.com>
2026-08-18 19:05:00 -0700

    Added footer in the loan11 page

--- src/components/loan11.jsx
+++ src/components/loan11.jsx
@@ -7,6 +7,7 @@ import Loan11iMasin2 from "./loan11-imasin2"
 import Loan11iMasin3 from "./loan11-imasin3"
 import Slayder6 from "./slayder6"
 import OnlineEvMobileBanking from "./onlineevmobilebanking"
+import 
 function Loan11(){
     const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
