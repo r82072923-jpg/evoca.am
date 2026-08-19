f9d7c75d5fec284da14065f1640a6756ba7369e2
Rubik <r82072923@gmail.com>
2026-08-19 12:10:49 -0700

    Added footer in the loan13 page

--- src/components/loan13.jsx
+++ src/components/loan13.jsx
@@ -8,6 +8,7 @@ import Loan13iMasin3 from "./loan13-imasin3"
 import Loan13iMasin4 from "./loan13-imasin4"
 import Slayder6 from "./slayder6"
 import OnlineEvMobileBanking from "./onlineevmobilebanking"
+import Footer from "./footer"
 function Loan13(){
     const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
@@ -29,6 +30,7 @@ function Loan13(){
         </div>
         <Slayder6></Slayder6>
         <OnlineEvMobileBanking></OnlineEvMobileBanking>
+        <Footer></Footer>
         </>
     )
 }
