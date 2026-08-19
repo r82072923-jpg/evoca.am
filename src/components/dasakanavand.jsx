9d3383e0aa7e6c2f618a7e1d84e20b9a1061046a
Rubik <r82072923@gmail.com>
2026-08-20 00:29:43 +0400

    Added slayder7 in the dasakanavand page

--- src/components/dasakanavand.jsx
+++ src/components/dasakanavand.jsx
@@ -4,6 +4,7 @@ import TopHeader from "./topHeader"
 import DasakanAvandiMasin from "./dasakanavand-imasin"
 import DasakanAvandiMasin2 from "./dasakanavand-imasin2"
 import DasakanAvandiMasin3 from "./dasakanavand-imasin3"
+import Slayder7 from "./slayder7"
 function DasakanAvand(){
     const [activeTab, setActiveTab] = useState('Ավանդի մասին');
     return(
@@ -19,6 +20,7 @@ function DasakanAvand(){
             <DasakanAvandiMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
           )}
         </div>
+        <Slayder7></Slayder7>
         </>
     )
 }
