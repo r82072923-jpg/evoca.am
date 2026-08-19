efbfc0406618234ff5390ff6583176c8521d6631
Rubik <r82072923@gmail.com>
2026-08-19 13:49:28 -0700

    Added loan15-imasin2 in the loan15 page

--- src/components/loan15.jsx
+++ src/components/loan15.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import LoansiTopHeader from "./loans-itopHeader"
 import Loan15iMasin from "./loan15-imasin"
+import Loan15iMasin2 from "./loan15-imasin2"
 function Loan15(){
+    const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <LoansiTopHeader></LoansiTopHeader>
         <Loan15iMasin></Loan15iMasin>
+        <div>
+          {activeTab === 'Վարկի մասին' && (
+            <Loan15iMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          {activeTab === 'Պայմաններ' && (
+            <Loan14iMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
