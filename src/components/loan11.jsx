9828ffc2eac5342b89e365ee9e5c3397dbd4f1b7
Rubik <r82072923@gmail.com>
2026-08-18 18:26:11 -0700

    Added loan11-imaisn2 in the loan11 page

--- src/components/loan11.jsx
+++ src/components/loan11.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader from "./topHeader"
 import LoansiTopHeader from "./loans-itopHeader"
-import Loan11iMasin from "./loan11-imaisn"
+import Loan11iMasin from "./loan11-imasin"
+import Loan11iMasin2 from "./loan11-imasin2"
 function Loan11(){
+    const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <LoansiTopHeader></LoansiTopHeader>
         <Loan11iMasin></Loan11iMasin>
+        <div>
+          {activeTab === 'Վարկի մասին' && (
+            <Loan11iMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          {activeTab === 'Պայմաններ' && (
+            <Loan10iMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
