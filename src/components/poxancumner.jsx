9796100ead1ab979815d41903819d52e6e22b761
Rubik <r82072923@gmail.com>
2026-09-03 20:57:30 +0400

    Added poxancumner-imasin2 in the poxancumner page

--- src/components/poxancumner.jsx
+++ src/components/poxancumner.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader from "./topHeader"
 import PoxancumneriMasin from "./poxancumner-imasin"
+import PoxancumneriMasin2 from "./poxancumner-imasin2"
 function Poxancumner(){
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
         <PoxancumneriMasin></PoxancumneriMasin>
+        <PoxancumneriMasin2></PoxancumneriMasin2>
         </>
     )
 }
