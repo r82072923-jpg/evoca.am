d1d05dca6050e823cfac4649b5d28654162e4207
Rubik <r82072923@gmail.com>
2026-09-03 19:29:07 +0400

    Added hashivner-imasin3 in the hashivner page

---
+++ src/components/hashivner-imasin3.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function HashivneriMasin3() {
+  return (
+    <div className="relative w-full h-[350px] sm:h-[400px] bg-[#3a007d] flex items-center justify-center overflow-hidden font-sans">
+      
+      <img
+        src="https://www.evoca.am/images-cache/loans/1/16142452902619/1920x527.jpg"
+        alt="Evocabank background"
+        className="absolute inset-0 w-full h-full object-cover z-0 opacity-90"
+      />
+
+
+      <div className="relative z-10 text-center px-6 max-w-4xl mx-auto">
+        <h1 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold leading-snug sm:leading-relaxed drop-shadow-md">
+          Գրավադրելով Ձեր ոսկյա իրերը՝<br className="hidden md:block" />
+          հնարավորություն կունենաք շատ արագ<br className="hidden md:block" />
+          հոգալու չնախատեսված ծախսերը։<br className="hidden md:block" />
+        </h1>
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default HashivneriMasin3;
