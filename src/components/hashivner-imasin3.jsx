2741209dfa8fc12ff06021fd12e0c7bdd208d45e
Rubik <r82072923@gmail.com>
2026-09-03 19:32:20 +0400

    Madxe changes in the hashivner-imasin3 code

--- src/components/hashivner-imasin3.jsx
+++ src/components/hashivner-imasin3.jsx
@@ -5,7 +5,7 @@ function HashivneriMasin3() {
     <div className="relative w-full h-[350px] sm:h-[400px] bg-[#3a007d] flex items-center justify-center overflow-hidden font-sans">
       
       <img
-        src="https://www.evoca.am/images-cache/loans/1/16142452902619/1920x527.jpg"
+        src="https://www.evoca.am/images-cache/menu/1/16111710051163/1920x530.jpg"
         alt="Evocabank background"
         className="absolute inset-0 w-full h-full object-cover z-0 opacity-90"
       />
@@ -13,9 +13,10 @@ function HashivneriMasin3() {
 
       <div className="relative z-10 text-center px-6 max-w-4xl mx-auto">
         <h1 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold leading-snug sm:leading-relaxed drop-shadow-md">
-          Գրավադրելով Ձեր ոսկյա իրերը՝<br className="hidden md:block" />
-          հնարավորություն կունենաք շատ արագ<br className="hidden md:block" />
-          հոգալու չնախատեսված ծախսերը։<br className="hidden md:block" />
+          Հաշիվներ բացելու նպատակով կարող եք<br className="hidden md:block" />
+          դիմել մեր Գլխամասային գրասենյակ կամ<br className="hidden md:block" />
+          ցանկացած մասնաճյուղ (բացառությամբ<br className="hidden md:block" />
+          «Էրեբունի» -ի):<br className="hidden md:block" />
         </h1>
       </div>
 
