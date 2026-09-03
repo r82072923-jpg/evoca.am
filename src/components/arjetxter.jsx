490fa1a355ac13e21c0ae4b4a7b2c6d82d91d436
Rubik <r82072923@gmail.com>
2026-09-03 23:41:04 +0400

    Added verjinnorutyunner4 in the arjetxter page

--- src/components/arjetxter.jsx
+++ src/components/arjetxter.jsx
@@ -3,6 +3,7 @@ import TopHeader from "./topHeader"
 import ArjetxteriMasin from "./arjetxter-imasin"
 import ArjetxteriMasin2 from "./arjetxter-imasin2"
 import ArjetxteriMasin3 from "./arjetxter-imasin3"
+import VerjinNorutyunner4 from "./verjinnorutyunner4"
 function Arjetxter(){
     return(
         <>
@@ -11,6 +12,7 @@ function Arjetxter(){
         <ArjetxteriMasin></ArjetxteriMasin>
         <ArjetxteriMasin2></ArjetxteriMasin2>
         <ArjetxteriMasin3></ArjetxteriMasin3>
+        <VerjinNorutyunner4></VerjinNorutyunner4>
         </>
     )
 }
