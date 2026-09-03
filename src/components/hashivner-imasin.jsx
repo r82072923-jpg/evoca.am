829092d082951419d66ec076fe09f60e7b4320af
Rubik <r82072923@gmail.com>
2026-09-03 19:17:54 +0400

    Made changes in the hashivner-imaisn code

--- src/components/hashivner-imasin.jsx
+++ src/components/hashivner-imasin.jsx
@@ -1,32 +1,28 @@
 function HashivneriMasin(){
     return(
         <>
-<section className="bg-[#f8f9fa] w-full py-16 px-6 md:px-16 lg:px-24 flex flex-col md:flex-row items-start justify-between gap-12">
-      
-      <div className="w-full md:w-1/2 flex flex-col pt-4">
-        <h2 className="text-[32px] md:text-[40px] font-bold text-[#2b2b2b] mb-6 leading-tight font-sans">
-          Հաշիվների բացում և<br />
-          սպասարկում
-        </h2>
-        
-        <p className="text-[#595959] text-[16px] md:text-[18px] leading-[1.8] font-sans">
-          Առաջարկում ենք բացել դրամային և արտարժութային
-          ընթացիկ բանկային հաշիվներ, որոնց սպասարկումն
-          իրականացնում ենք մեր սակագների համաձայն։ Մեզ մոտ
-          հաշիվներ կարող են բացել Հայաստանի ռեզիդենտ և ոչ
-          ռեզիդենտ ֆիզիկական անձիք։
-        </p>
-      </div>
-
-      <div className="w-full md:w-1/2">
-        <img
-          src="https://www.evoca.am/images-cache/menu/1/16111691720299/780x585.jpg"
-          alt="Evocabank մասնաճյուղ"
-          className="w-full h-auto object-cover"
-        />
-      </div>
-      
-</section>
+        <section className="bg-[#f8f9fa] w-full py-16 px-6 md:px-16 lg:px-24 flex flex-col md:flex-row items-start justify-between gap-12">
+            <div className="w-full md:w-1/2 flex flex-col pt-4">
+                <h2 className="text-[32px] md:text-[40px] font-bold text-[#2b2b2b] mb-6 leading-tight font-sans">
+                Հաշիվների բացում և<br />
+                սպասարկում
+                </h2>
+                <p className="text-[#595959] text-[16px] md:text-[18px] leading-[1.8] font-sans">
+                Առաջարկում ենք բացել դրամային և արտարժութային
+                ընթացիկ բանկային հաշիվներ, որոնց սպասարկումն
+                իրականացնում ենք մեր սակագների համաձայն։ Մեզ մոտ
+                հաշիվներ կարող են բացել Հայաստանի ռեզիդենտ և ոչ
+                ռեզիդենտ ֆիզիկական անձիք։
+                </p>
+            </div>
+            <div className="w-full md:w-1/2">
+                <img
+                src="https://www.evoca.am/images-cache/menu/1/16111691720299/780x585.jpg"
+                alt="Evocabank մասնաճյուղ"
+                className="w-full h-auto object-cover"
+                />
+            </div>
+        </section>
         </>
     )
 }
--- src/components/hashivner.jsx
+++ src/components/hashivner.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader from "./topHeader"
+import HashivneriMasin from "./hashivner-imasin"
 function Hashivner(){
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
+        <HashivneriMasin></HashivneriMasin>
         </>
     )
 }
