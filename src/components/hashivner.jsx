51b833cb6f4e850d0ab26a5ef07c9df13bb84d54
Rubik <r82072923@gmail.com>
2026-09-03 19:29:55 +0400

    Made changes in the hashivner page

--- src/components/hashivner.jsx
+++ src/components/hashivner.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader from "./topHeader"
 import HashivneriMasin from "./hashivner-imasin"
 import HashivneriMasin2 from "./hashivner-imasin2"
+import HashivneriMasin3 from "./hashivner-imasin3"
 function Hashivner(){
     return(
         <>
@@ -9,6 +10,7 @@ function Hashivner(){
         <TopHeader></TopHeader>
         <HashivneriMasin></HashivneriMasin>
         <HashivneriMasin2></HashivneriMasin2>
+        <HashivneriMasin3></HashivneriMasin3>
         </>
     )
 }
