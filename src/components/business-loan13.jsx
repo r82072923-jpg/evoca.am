01a11670090f6c42d8a0cc9e52e7fd04a5d32715
Rubik <r82072923@gmail.com>
2026-09-04 15:50:17 +0400

    Made changes in the business-loan13 code

--- src/components/business-loan13.jsx
+++ src/components/business-loan13.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader from "./topHeader"
+import BusinessLoan13iMasin from "./business-loan13-imasin"
 function BusinessLoan13(){
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
+        <BusinessLoan13iMasin></BusinessLoan13iMasin>
         </>
     )
 }
