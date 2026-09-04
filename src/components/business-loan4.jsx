a66ba052f3f2120f2c576412417b0183ba4f0971
Rubik <r82072923@gmail.com>
2026-09-04 20:43:41 +0400

    Added footer in the business-loan4 page

--- src/components/business-loan4.jsx
+++ src/components/business-loan4.jsx
@@ -5,6 +5,7 @@ import BusinessLoan4iMasin from "./business-loan4-imasin"
 import BusinessLoan4iMasin2 from "./business-loan4-imasin2"
 import Slayder4 from "./slayder4"
 import OnlineEvMobileBanking from "./onlineevmobilebanking"
+import Footer from "./footer"
 function BusinessLoan4(){
     const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
@@ -19,6 +20,7 @@ function BusinessLoan4(){
         </div>
         <Slayder4></Slayder4>
         <OnlineEvMobileBanking></OnlineEvMobileBanking>
+        <Footer></Footer>
         </>
     )
 }
