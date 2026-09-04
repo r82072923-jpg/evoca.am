0ac84226e728877d9bb82c2c8be9e23225d98d0d
Rubik <r82072923@gmail.com>
2026-09-04 19:10:23 +0400

    Added business-loan3-imasin in the business-loan3 page

--- src/App.jsx
+++ src/App.jsx
@@ -80,6 +80,7 @@ import Arjetxter from "./components/arjetxter.jsx"
 import BusinessLoan13 from "./components/business-loan13.jsx"
 import BusinessLoan12 from "./components/business-loan12.jsx"
 import BusinessLoan8 from "./components/business-loan8.jsx"
+import BusinessLoan3 from "./components/business-loan3.jsx"
 import { useState } from "react"
 import { Routes, Route } from "react-router-dom";
 
@@ -187,6 +188,7 @@ function App() {
         <Route path="/business-loan13" element={<BusinessLoan13/>}></Route>
         <Route path="/business-loan12" element={<BusinessLoan12/>}></Route>
         <Route path="/business-loan8" element={<BusinessLoan8/>}></Route>
+        <Route path="/business-loan3" element={<BusinessLoan3/>}></Route>
       </Routes>
     </>
   )
---
+++ src/components/business-loan3.jsx
@@ -0,0 +1,13 @@
+import Header from "./header"
+import TopHeader2 from "./topHeader2"
+import BusinessLoan3iMasin from "./business-loan3-imasin"
+function BusinessLoan3(){
+    return(
+        <>
+        <Header></Header>
+        <TopHeader2></TopHeader2>
+        <BusinessLoan3iMasin></BusinessLoan3iMasin>
+        </>
+    )
+}
+export default BusinessLoan3
