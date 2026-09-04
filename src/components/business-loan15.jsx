441bbd76fb46080abc85d17d9ac0cd2b4f408176
Rubik <r82072923@gmail.com>
2026-09-04 22:08:32 +0400

    Made changes in the business-loan15 code

--- src/components/business-loan15.jsx
+++ src/components/business-loan15.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react"
 import Header from "./header"
 import TopHeader2 from "./topHeader2"
 import BusinessLoan15iMasin from "./business-loan15-imasin"
+import BusinessLoan15iMasin2 from "./business-loan15-imasin2"
 function BusinessLoan15(){
+    const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
         <>
         <Header></Header>
         <TopHeader2></TopHeader2>
         <BusinessLoan15iMasin></BusinessLoan15iMasin>
+        <div>
+          {activeTab === 'Վարկի մասին' && (
+            <BusinessLoan15iMasin2 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+          {activeTab === 'Պայմաններ' && (
+            <BusinessLoan2iMasin3 activeTab={activeTab} setActiveTab={setActiveTab} />
+          )}
+        </div>
         </>
     )
 }
