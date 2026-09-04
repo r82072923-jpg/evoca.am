eb102789058a1019fb0b90f3c8633587e6e05d9b
Rubik <r82072923@gmail.com>
2026-09-04 16:52:18 +0400

    Added header and topHeader in the business-loan12 page

---
+++ src/components/business-loan12.jsx
@@ -0,0 +1,11 @@
+import Header from "./header"
+import TopHeader from "./topHeader"
+function BusinessLoan12(){
+    return(
+        <>
+        <Header></Header>
+        <TopHeader></TopHeader>
+        </>
+    )
+}
+export default BusinessLoan12
