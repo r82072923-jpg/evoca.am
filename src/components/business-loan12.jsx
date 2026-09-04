c5a847985a4827a206fcf3376da7d238c753f0ba
Rubik <r82072923@gmail.com>
2026-09-04 16:56:43 +0400

    Added business-loan12-imasin in the business-loan12 page

--- src/components/business-loan12.jsx
+++ src/components/business-loan12.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader from "./topHeader"
+import BusinessLoan12iMasin from "./business-loan12-imasin"
 function BusinessLoan12(){
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
+        <BusinessLoan12iMasin></BusinessLoan12iMasin>
         </>
     )
 }
