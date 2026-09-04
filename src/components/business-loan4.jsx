fb8f87d87d6ab531f0bc1c611f103769425b77dc
Rubik <r82072923@gmail.com>
2026-09-04 20:30:34 +0400

    Added business-loan4-imasin in the business-loan4 page

--- src/components/business-loan4.jsx
+++ src/components/business-loan4.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader2 from "./topHeader2"
+import BusinessLoan4iMasin from "./business-loan4-imasin"
 function BusinessLoan4(){
     return(
         <>
         <Header></Header>
         <TopHeader2></TopHeader2>
+        <BusinessLoan4iMasin></BusinessLoan4iMasin>
         </>
     )
 }
