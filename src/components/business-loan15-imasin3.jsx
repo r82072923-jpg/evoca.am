d5d129e3102841c6831388e57ff565eab037d973
Rubik <r82072923@gmail.com>
2026-09-05 14:22:25 +0400

    Made chnages in the business-loan15-imasin3 code

--- src/components/business-loan15-imasin3.jsx
+++ src/components/business-loan15-imasin3.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { collection, addDoc } from 'firebase/firestore';
-import { db } from './firebaseConfig';
+import { db } from './firebaseConfog';
 
 const loanData = {
   tableData: [
