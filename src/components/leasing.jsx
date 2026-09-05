e076c6d0b9078aa0da6c5cf16d85cb84b7615c3e
Rubik <r82072923@gmail.com>
2026-09-05 16:36:43 +0400

    Added footerBottom in the leasong page

--- src/components/leasing.jsx
+++ src/components/leasing.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader2 from "./topHeader2"
 import LeasingiMasin from "./leasing-imasin"
 import Footer from "./footer"
+import FooterBottom from "./footerBottom"
 function Leasing(){
     return(
         <>
@@ -9,6 +10,7 @@ function Leasing(){
         <TopHeader2></TopHeader2>
         <LeasingiMasin></LeasingiMasin>
         <Footer></Footer>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
