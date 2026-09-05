4293ee77cee3ccd2714f570f3d0d7559fd3b23d1
Rubik <r82072923@gmail.com>
2026-09-05 17:39:43 +0400

    Added ayl-imasin in the ayl page

--- src/components/ayl.jsx
+++ src/components/ayl.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
-import TopHeader2 from "./topHeader2"
+import TopHeader2 from "./topHeader2" 
+import AyliMasin from "./ayl-imasin"
 function Ayl(){
     return(
         <>
         <Header></Header>
         <TopHeader2></TopHeader2>
+        <AyliMasin></AyliMasin>
         </>
     )
 }
