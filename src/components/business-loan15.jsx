a440139e613acc5a065c439e5b5a174eb3a71784
Rubik <r82072923@gmail.com>
2026-09-05 15:35:36 +0400

    Added onlineEvMobileBanking in the business-loan15 page

--- src/components/business-loan15.jsx
+++ src/components/business-loan15.jsx
@@ -6,6 +6,7 @@ import BusinessLoan15iMasin2 from "./business-loan15-imasin2"
 import BusinessLoan15iMasin3 from "./business-loan15-imasin3"
 import BusinessLoan15iMasin4 from "./business-loan15-imasin4"
 import Slayder4 from "./slayder4"
+import OnlineEvMobileBanking from "./onlineevmobilebanking"
 function BusinessLoan15(){
     const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
@@ -25,6 +26,7 @@ function BusinessLoan15(){
           )}
         </div>
         <Slayder4></Slayder4>
+        <OnlineEvMobileBanking></OnlineEvMobileBanking>
         </>
     )
 }
