7898ec07588064c59bfb8ec7177c01a581c8900f
Rubik <r82072923@gmail.com>
2026-09-05 16:35:47 +0400

    Added fooetr in the leasing page

--- src/components/leasing.jsx
+++ src/components/leasing.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader2 from "./topHeader2"
 import LeasingiMasin from "./leasing-imasin"
+import Footer from "./footer"
 function Leasing(){
     return(
         <>
         <Header></Header>
         <TopHeader2></TopHeader2>
         <LeasingiMasin></LeasingiMasin>
+        <Footer></Footer>
         </>
     )
 }
