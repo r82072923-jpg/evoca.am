67652526d07045196f215682e0e108cfe835f5ca
Rubik <r82072923@gmail.com>
2026-09-05 19:43:31 +0400

    Added verjinnorutyunner4 in the arevtrifinansavorum page

--- src/components/arevtrifinansavorum.jsx
+++ src/components/arevtrifinansavorum.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader2 from "./topHeader2"
 import ArevtriFinansavorumiMasin from "./arevtrifinansavorum-imasin"
+import VerjinNorutyunner4 from "./verjinnorutyunner4"
 function ArevtriFinansavorum(){
     return(
         <>
         <Header></Header>
         <TopHeader2></TopHeader2>
         <ArevtriFinansavorumiMasin></ArevtriFinansavorumiMasin>
+        <VerjinNorutyunner4></VerjinNorutyunner4>
         </>
     )
 }
