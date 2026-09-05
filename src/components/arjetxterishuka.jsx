a09687c239bb4156cee24e41a05d12514c6e36f8
Rubik <r82072923@gmail.com>
2026-09-05 18:49:14 +0400

    Added footer in the arjetxterishuka page

--- src/components/arjetxterishuka.jsx
+++ src/components/arjetxterishuka.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader2 from "./topHeader2"
 import ArjetxteriShukaiMasin from "./arjetxterishuka-imasin"
+import Footer from "./footer"
 function ArjetxteriShuka(){
     return(
         <>
         <Header></Header>
         <TopHeader2></TopHeader2>
         <ArjetxteriShukaiMasin></ArjetxteriShukaiMasin>
+        <Footer></Footer>
         </>
     )
 }
