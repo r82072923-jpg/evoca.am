2bac9cf6f713f532e0f684e1cc30c81429640ced
Rubik <r82072923@gmail.com>
2026-09-05 22:53:18 +0400

    Added footerBottom in the haytararutyunner page

--- src/components/haytararutyunner.jsx
+++ src/components/haytararutyunner.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader3 from "./topHeader3"
 import Slayder8 from "./slayder8"
 import Footer from "./footer"
+import FooterBottom from "./footerBottom"
 function Haytararutyunner(){
     return(
         <>
@@ -9,6 +10,7 @@ function Haytararutyunner(){
         <TopHeader3></TopHeader3>
         <Slayder8></Slayder8>
         <Footer></Footer>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
