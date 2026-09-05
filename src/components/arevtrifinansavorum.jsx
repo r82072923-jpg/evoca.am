4cf7066941b4d862dfb3a3c79993d225b4565bdf
Rubik <r82072923@gmail.com>
2026-09-05 19:12:37 +0400

    Added arevtrifinanasavorum-imasin  in the arevtrifinansavorum page

--- src/components/arevtrifinansavorum.jsx
+++ src/components/arevtrifinansavorum.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader2 from "./topHeader2"
+import ArevtriFinansavorumiMasin from "./arevtrifinansavorum-imasin"
 function ArevtriFinansavorum(){
     return(
         <>
         <Header></Header>
         <TopHeader2></TopHeader2>
+        <ArevtriFinansavorumiMasin></ArevtriFinansavorumiMasin>
         </>
     )
 }
