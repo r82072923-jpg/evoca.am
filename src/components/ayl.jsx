2946671e68376aa6e898e383c8af463e2f195e53
Rubik <r82072923@gmail.com>
2026-09-05 18:14:38 +0400

    Added onlineEvMobileBanking in the ayl page

--- src/components/ayl.jsx
+++ src/components/ayl.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader2 from "./topHeader2" 
 import AyliMasin from "./ayl-imasin"
 import AyliMasin2 from "./ayl-imasin2"
+import OnlineEvMobileBanking from "./onlineevmobilebanking"
 function Ayl(){
     return(
         <>
@@ -9,6 +10,7 @@ function Ayl(){
         <TopHeader2></TopHeader2>
         <AyliMasin></AyliMasin>
         <AyliMasin2></AyliMasin2>
+        <OnlineEvMobileBanking></OnlineEvMobileBanking>
         </>
     )
 }
