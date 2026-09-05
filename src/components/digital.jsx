404271c24b04beb107ac88aa7937ef814fc4ea79
Rubik <r82072923@gmail.com>
2026-09-05 17:32:40 +0400

    Added onlineEvMobileBanking in the digital page

--- src/components/digital.jsx
+++ src/components/digital.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader2 from "./topHeader2"
 import DigitaliMasin from "./digital-imasin"
 import DigitaliMasin2 from "./digital-imasin2"
+import OnlineEvMobileBanking from "./onlineevmobilebanking"
 function Digital(){
     return(
         <>
@@ -9,6 +10,7 @@ function Digital(){
         <TopHeader2></TopHeader2>
         <DigitaliMasin></DigitaliMasin>
         <DigitaliMasin2></DigitaliMasin2>
+        <OnlineEvMobileBanking></OnlineEvMobileBanking>
         </>
     )
 }
