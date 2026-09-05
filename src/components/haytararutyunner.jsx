edf032a7724f25fea202985a9ce467877705effe
Rubik <r82072923@gmail.com>
2026-09-05 22:51:56 +0400

    Added footer in the haytararutyunner page

--- src/components/haytararutyunner.jsx
+++ src/components/haytararutyunner.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader3 from "./topHeader3"
 import Slayder8 from "./slayder8"
+import Footer from "./footer"
 function Haytararutyunner(){
     return(
         <>
         <Header></Header>
         <TopHeader3></TopHeader3>
         <Slayder8></Slayder8>
+        <Footer></Footer>
         </>
     )
 }
