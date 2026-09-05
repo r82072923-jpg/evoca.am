5df888f701b7fc33fec74b718e5da8e25a0d59f8
Rubik <r82072923@gmail.com>
2026-09-05 23:14:37 +0400

    Added footer in the hashvetvutyunner page

--- src/components/hashvetvutyunner.jsx
+++ src/components/hashvetvutyunner.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader3 from "./topHeader3"
 import HashvetvutyunneriMasin from "./hashvetvutyunner-imasin"
+import Footer from "./footer"
 function Hashvetvutyunner(){
     return(
         <>
         <Header></Header>
         <TopHeader3></TopHeader3> 
-        <HashvetvutyunneriMasin></HashvetvutyunneriMasin>    
+        <HashvetvutyunneriMasin></HashvetvutyunneriMasin>   
+        <Footer></Footer> 
         </>
     )
 }
