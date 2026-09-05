7df98efdcb75283d50794d4709238ce0df24294d
Rubik <r82072923@gmail.com>
2026-09-05 21:00:33 +0400

    Added header and topHeader2 in the haytararutyunner page

--- src/App.jsx
+++ src/App.jsx
@@ -88,6 +88,7 @@ import Leasing from "./components/leasing.jsx"
 import Digital from "./components/digital.jsx"
 import Ayl from "./components/ayl.jsx"
 import ArjetxteriShuka from "./components/arjetxterishuka.jsx"
+import Haytararutyunner from "./components/haytararutyunner.jsx"
 import ArevtriFinansavorum from "./components/arevtrifinansavorum.jsx"
 import { useState } from "react"
 import { Routes, Route } from "react-router-dom";
@@ -205,6 +206,7 @@ function App() {
         <Route path="/other" element={<Ayl/>}></Route>
         <Route path="/securities" element={<ArjetxteriShuka/>}></Route>
         <Route path="/trade-finance" element={<ArevtriFinansavorum/>}></Route>
+        <Route path="/announcements" element={<Haytararutyunner/>}></Route>
       </Routes>
     </>
   )
