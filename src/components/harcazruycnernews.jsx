223d34c6a0c3911838267e9628df5d2eb0621393
Rubik <r82072923@gmail.com>
2026-09-06 15:28:34 +0400

    Added footer in the harcazruycnernews page

--- src/components/harcazruycnernews.jsx
+++ src/components/harcazruycnernews.jsx
@@ -3,6 +3,7 @@ import TopHeader4 from "./topHeader4"
 import NewsiBajinner from "./news-ibajinner"
 import HarcazruycnerNewsiMasin from "./harcazruycnernews-imasin"
 import HarcazruycnerNewsiMasin2 from "./harcazruycnernews-imasin2"
+import Footer from "./footer"
 function HarcazruycnerNews(){
     return(
         <>
@@ -11,6 +12,7 @@ function HarcazruycnerNews(){
         <NewsiBajinner></NewsiBajinner>
         <HarcazruycnerNewsiMasin></HarcazruycnerNewsiMasin>
         <HarcazruycnerNewsiMasin2></HarcazruycnerNewsiMasin2>
+        <Footer></Footer>
         </>
     )
 }
