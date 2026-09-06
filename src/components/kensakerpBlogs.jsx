6b84af9309a41e0dd0406b92ce1fbb562992de18
Rubik <r82072923@gmail.com>
2026-09-06 20:59:44 +0400

    Added blogibajinner and kensakerpBlogs-imasin page

--- src/components/kensakerpBlogs.jsx
+++ src/components/kensakerpBlogs.jsx
@@ -1,10 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import BlogiBajinner from "./blog-iBajinner"
+import KensakerpBlogsiMasin from "./kensakerpBlogs-imasin"
 function KensakerpBlogs(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
+        <BlogiBajinner></BlogiBajinner>
+        <KensakerpBlogsiMasin></KensakerpBlogsiMasin>
         </>
     )
 }
