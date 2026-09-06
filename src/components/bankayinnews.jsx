406fb5947f4c77a722a444f5efb8ddc0287b1c37
Rubik <r82072923@gmail.com>
2026-09-06 13:36:56 +0400

    Added bankayinnews-imasin in the bankayinnews page

--- src/components/bankayinnews.jsx
+++ src/components/bankayinnews.jsx
@@ -1,10 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import NewsiBajinner from "./news-ibajinner"
+import BankayinNewsiMasin from "./bankayinnnews-imasin"
 function BankayinNews(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
+        <NewsiBajinner></NewsiBajinner>
+        <BankayinNewsiMasin></BankayinNewsiMasin>
         </>
     )
 }
