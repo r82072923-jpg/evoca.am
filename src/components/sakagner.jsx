e7073aba418d5a29d7ee8de74d527e141f50285f
Rubik <r82072923@gmail.com>
2026-09-06 12:53:26 +0400

    Added footer in the ssakagner page

--- src/components/sakagner.jsx
+++ src/components/sakagner.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader3 from "./topHeader3"
 import SakagneriMasin from "./sakagner-imasin"
+import Footer from "./footer"
 function Sakagner(){
     return(
         <>
         <Header></Header>
         <TopHeader3></TopHeader3>
         <SakagneriMasin></SakagneriMasin>
+        <Footer></Footer>
         </>
     )
 }
