f5af08668b1a94a18221182720f6216fa4abf423
Rubik <r82072923@gmail.com>
2026-09-06 22:40:28 +0400

    Added footer in the careers page

--- src/components/careers.jsx
+++ src/components/careers.jsx
@@ -3,6 +3,7 @@ import TopHeader5 from "./topHeader5"
 import Careers5 from "./careers5"
 import Careers6 from "./careers6"
 import Careers7 from "./careers7"
+import Footer from "./footer"
 function Careers(){
     return(
         <>
@@ -11,6 +12,7 @@ function Careers(){
         <Careers5></Careers5>
         <Careers6></Careers6>
         <Careers7></Careers7>
+        <Footer></Footer>
         </>
     )
 }
