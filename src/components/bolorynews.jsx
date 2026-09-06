42297e5da9e30c0ccc315e5c1ed5a3a8fa7c57ca
Rubik <r82072923@gmail.com>
2026-09-06 20:06:25 +0400

    Added footer in the bolorynews page

--- src/components/bolorynews.jsx
+++ src/components/bolorynews.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import NewsiBajinner from "./news-ibajinner"
 import BoloryNewsiMasin from "./bolorynews-imasin"
+import Footer from "./footer"
 function BoloryNews(){
     return(
         <>
@@ -9,6 +10,7 @@ function BoloryNews(){
         <TopHeader4></TopHeader4>
         <NewsiBajinner></NewsiBajinner>
         <BoloryNewsiMasin></BoloryNewsiMasin>
+        <Footer></Footer>
         </>
     )
 }
