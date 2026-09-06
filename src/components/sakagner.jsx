0f8cebe679242d5f9954984530e4382b5bf07f61
Rubik <r82072923@gmail.com>
2026-09-06 12:54:20 +0400

    Added footerBottom in the sakagner page

--- src/components/sakagner.jsx
+++ src/components/sakagner.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader3 from "./topHeader3"
 import SakagneriMasin from "./sakagner-imasin"
 import Footer from "./footer"
+import FooterBottom from "./footerBottom"
 function Sakagner(){
     return(
         <>
@@ -9,6 +10,7 @@ function Sakagner(){
         <TopHeader3></TopHeader3>
         <SakagneriMasin></SakagneriMasin>
         <Footer></Footer>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
