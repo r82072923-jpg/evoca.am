eed8d04113299bac6660b39004ffc8e62fb8b1dc
Rubik <r82072923@gmail.com>
2026-09-06 21:26:34 +0400

    Added blogiBajinner and nerdrumnerBlogs-imasin in the nerdrumnerBlogs page

--- src/components/nerdrumnerBlogs.jsx
+++ src/components/nerdrumnerBlogs.jsx
@@ -1,10 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import BlogiBajinner from "./blog-iBajinner"
+import NerdrumnerBlogsiMasin from "./nerdrumnerBlogs-imasin"
 function NerdrumnerBlogs(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
+        <BlogiBajinner></BlogiBajinner>
+        <NerdrumnerBlogsiMasin></NerdrumnerBlogsiMasin>
         </>
     )
 }
