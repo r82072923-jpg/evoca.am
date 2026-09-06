681f1e5152d4dafa6b6ea8029a39bf9c64170f59
Rubik <r82072923@gmail.com>
2026-09-06 15:11:35 +0400

    Added footerBottom in the bankayinnews page

--- src/components/bankayinnews.jsx
+++ src/components/bankayinnews.jsx
@@ -4,6 +4,7 @@ import NewsiBajinner from "./news-ibajinner"
 import BankayinNewsiMasin from "./bankayinnews-imasin"
 import BankayinNewsiMasin2 from "./bankayinnews-imasin2"
 import Footer from "./footer"
+import FooterBottom from "./footerBottom"
 function BankayinNews(){
     return(
         <>
@@ -13,6 +14,7 @@ function BankayinNews(){
         <BankayinNewsiMasin></BankayinNewsiMasin>
         <BankayinNewsiMasin2></BankayinNewsiMasin2>
         <Footer></Footer>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
