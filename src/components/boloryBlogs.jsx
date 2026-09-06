83a0c0469af2be9d89bbf8ca63beed4a3e2513a3
Rubik <r82072923@gmail.com>
2026-09-06 21:56:49 +0400

    Added blogiBajinner and BoloryBlogsiMasin in the boloryBlogs page

--- src/components/boloryBlogs.jsx
+++ src/components/boloryBlogs.jsx
@@ -1,10 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import BlogiBajinner from "./blog-iBajinner"
+import BoloryBlogsiMasin from "./boloryBlogs-imasin"
 function BoloryBlogs(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
+        <BlogiBajinner></BlogiBajinner>
+        <BoloryBlogsiMasin></BoloryBlogsiMasin>
         </>
     )
 }
