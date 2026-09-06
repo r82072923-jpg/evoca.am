7a0b95a43fe280cdb9d5930455a9b473e35a08d1
Rubik <r82072923@gmail.com>
2026-09-06 17:49:27 +0400

    Added footerBottom in the kensakerpnews page

--- src/components/kensakerpnews.jsx
+++ src/components/kensakerpnews.jsx
@@ -4,6 +4,7 @@ import NewsiBajinner from "./news-ibajinner"
 import KensakerpNewsiMasin from "./kensakerpnews-imasin"
 import KensakerpNewsiMasin2 from "./kensakerpnews-imasin2"
 import Footer from "./footer"
+import FooterBottom from "./footerBottom"
 function KensakerpNews(){
     return(
         <>
@@ -13,6 +14,7 @@ function KensakerpNews(){
         <KensakerpNewsiMasin></KensakerpNewsiMasin>
         <KensakerpNewsiMasin2></KensakerpNewsiMasin2>
         <Footer></Footer>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
