ecc22ce7c7a84629c04041c728e3dd4a9e86c6b0
Rubik <r82072923@gmail.com>
2026-09-06 18:28:45 +0400

    Added footer in the mrcanaknernews page

--- src/components/mrcanaknernews.jsx
+++ src/components/mrcanaknernews.jsx
@@ -3,6 +3,7 @@ import TopHeader4 from "./topHeader4"
 import NewsiBajinner from "./news-ibajinner"
 import MrcanaknerNewsiMasin from "./mrcanaknernews-imasin"
 import MrcanaknerNewsiMasin2 from "./mrcanaknernews-imasin2"
+import Footer from "./footer"
 function MrcaanaknerNews(){
     return(
         <>
@@ -11,6 +12,7 @@ function MrcaanaknerNews(){
         <NewsiBajinner></NewsiBajinner>
         <MrcanaknerNewsiMasin></MrcanaknerNewsiMasin>
         <MrcanaknerNewsiMasin2></MrcanaknerNewsiMasin2>
+        <Footer></Footer>
         </>
     )
 }
