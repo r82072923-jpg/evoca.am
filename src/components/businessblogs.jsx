e3981b0aedfac67b33497f37322fbca8a42b982b
Rubik <r82072923@gmail.com>
2026-09-06 20:44:56 +0400

    Added footer in the businessblogs page

--- src/components/businessblogs.jsx
+++ src/components/businessblogs.jsx
@@ -3,6 +3,7 @@ import TopHeader4 from "./topHeader4"
 import BlogiBajinner from "./blog-iBajinner"
 import BusinessBlogsiMasin from "./businessblogs-imasin"
 import BusinessBlogsiMasin2 from "./businessblogs-imasin2"
+import Footer from "./footer"
 function BusinessBlogs(){
     return(
         <>
@@ -11,6 +12,7 @@ function BusinessBlogs(){
         <BlogiBajinner></BlogiBajinner>
         <BusinessBlogsiMasin></BusinessBlogsiMasin>
         <BusinessBlogsiMasin2></BusinessBlogsiMasin2>
+        <Footer></Footer>
         </>
     )
 }
