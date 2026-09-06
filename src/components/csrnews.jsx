f970af4851f0748597481373d171ab0ba569cc44
Rubik <r82072923@gmail.com>
2026-09-06 18:39:56 +0400

    Added news-ibajinner and csrnews-imasin in the csrnews page

--- src/components/csrnews.jsx
+++ src/components/csrnews.jsx
@@ -1,10 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import NewsiBajinner from "./news-ibajinner"
+import CsrNewsiMasin from "./csrnews-imasin"
 function CsrNews(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
+        <NewsiBajinner></NewsiBajinner>
+        <CsrNewsiMasin></CsrNewsiMasin>
         </>
     )
 }
