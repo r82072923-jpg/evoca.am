30bfa214cd2620cdcec7560d4c74f14962ad8633
Rubik <r82072923@gmail.com>
2026-09-06 22:06:28 +0400

    Added careers5 in the careers page

--- src/components/careers.jsx
+++ src/components/careers.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader5 from "./topHeader5"
+import Careers5 from "./careers5"
 function Careers(){
     return(
         <>
         <Header></Header>
         <TopHeader5></TopHeader5>
+        <Careers5></Careers5>
         </>
     )
 }
---
+++ src/components/careers5.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Careers5() {
+  return (
+    <div className="relative w-full h-screen min-h-[600px] overflow-hidden bg-pink-50 font-sans">
+      
+      <img 
+        src="https://www.evoca.am/images-cache/menu/1/16194611253542/1920x634.jpg" 
+        alt="Creative Evoca Background" 
+        className="absolute inset-0 w-full h-full object-center"
+      />
+
+      <div className="absolute bottom-0 left-0 md:left-12 lg:left-24 bg-[#F8F9FB] w-full max-w-[550px] p-8 md:p-12 shadow-2xl z-10 rounded-tr-md">
+        <h2 className="text-[#1A1A24] text-3xl font-extrabold mb-4 tracking-tight">
+          Աշխատանք Evoca-ում
+        </h2>
+      </div>
+
+
+    </div>
+  );
+}
+export default Careers5
