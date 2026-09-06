3c709df3645ad23f37e6735adbf09270f00db644
Rubik <r82072923@gmail.com>
2026-09-06 17:00:43 +0400

    Added news-ibajinner and kensakerpnews-imasin in the kensakerpnews page

--- src/components/kensakerpnews.jsx
+++ src/components/kensakerpnews.jsx
@@ -1,10 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import NewsiBajinner from "./news-ibajinner"
+import KensakerpNewsiMasin from "./kensakerpnews-imasin"
 function KensakerpNews(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
+        <NewsiBajinner></NewsiBajinner>
+        <KensakerpNewsiMasin></KensakerpNewsiMasin>
         </>
     )
 }
