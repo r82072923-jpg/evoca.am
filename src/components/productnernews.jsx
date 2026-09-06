c21b41b173aa521d664f9090071c64690c52ae12
Rubik <r82072923@gmail.com>
2026-09-06 15:35:52 +0400

    Added news-ibajinner and productnernews-imasin in the producternews page

--- src/components/productnernews.jsx
+++ src/components/productnernews.jsx
@@ -1,10 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import NewsiBajinner from "./news-ibajinner"
+import ProductnerNewsiMasin from "./productnernews-imasin"
 function ProductnerNews(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
+        <NewsiBajinner></NewsiBajinner>
+        <ProductnerNewsiMasin></ProductnerNewsiMasin>
         </>
     )
 }
