d617c00bb8bf5ca055570696de523de6d080c633
Rubik <r82072923@gmail.com>
2026-09-06 18:29:35 +0400

    Added footerBottom in the mrcanaknernews page

--- src/components/mrcanaknernews.jsx
+++ src/components/mrcanaknernews.jsx
@@ -4,6 +4,7 @@ import NewsiBajinner from "./news-ibajinner"
 import MrcanaknerNewsiMasin from "./mrcanaknernews-imasin"
 import MrcanaknerNewsiMasin2 from "./mrcanaknernews-imasin2"
 import Footer from "./footer"
+import FooterBottom from "./footerBottom"
 function MrcaanaknerNews(){
     return(
         <>
@@ -13,6 +14,7 @@ function MrcaanaknerNews(){
         <MrcanaknerNewsiMasin></MrcanaknerNewsiMasin>
         <MrcanaknerNewsiMasin2></MrcanaknerNewsiMasin2>
         <Footer></Footer>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
