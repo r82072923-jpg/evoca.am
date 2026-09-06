32a6f858a4899cd08de3a61eaf892480ce26beea
Rubik <r82072923@gmail.com>
2026-09-06 19:40:48 +0400

    Added aylnews-imasin2 in the aylnews page

--- src/components/aylnews.jsx
+++ src/components/aylnews.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import NewsiBajinner from "./news-ibajinner"
 import AylNewsiMasin from "./aylnews-imasin"
+import AylNewsiMasin2 from "./aylnews-imasin2"
 function AylNews(){
     return(
         <>
@@ -9,6 +10,7 @@ function AylNews(){
         <TopHeader4></TopHeader4>
         <NewsiBajinner></NewsiBajinner>
         <AylNewsiMasin></AylNewsiMasin>
+        <AylNewsiMasin2></AylNewsiMasin2>
         </>
     )
 }
