9bb783c558e9c35789d88e96e89bd55896b461ba
Rubik <r82072923@gmail.com>
2026-09-06 20:30:56 +0400

    Added businessblogs-imasin in the businessblog page

--- src/components/businessblogs.jsx
+++ src/components/businessblogs.jsx
@@ -1,10 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import BlogiBajinner from "./blog-iBajinner"
+import BusinessBlogsiMasin from "./businessblogs-imasin"
 function BusinessBlogs(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
+        <BlogiBajinner></BlogiBajinner>
+        <BusinessBlogsiMasin></BusinessBlogsiMasin>
         </>
     )
 }
