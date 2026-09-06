943400d635ac7a65fe37bd4e97e6bb4148d58e5a
Rubik <r82072923@gmail.com>
2026-09-06 21:35:34 +0400

    Added footerBottom in the nerdrumnerBlogs page

--- src/components/nerdrumnerBlogs.jsx
+++ src/components/nerdrumnerBlogs.jsx
@@ -4,6 +4,7 @@ import BlogiBajinner from "./blog-iBajinner"
 import NerdrumnerBlogsiMasin from "./nerdrumnerBlogs-imasin"
 import NerdrumnerBlogsiMasin2 from "./nerdrumnerBlogs-imasin2"
 import Footer from "./footer"
+import FooterBottom from "./footerBottom"
 function NerdrumnerBlogs(){
     return(
         <>
@@ -13,6 +14,7 @@ function NerdrumnerBlogs(){
         <NerdrumnerBlogsiMasin></NerdrumnerBlogsiMasin>
         <NerdrumnerBlogsiMasin2></NerdrumnerBlogsiMasin2>
         <Footer></Footer>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
