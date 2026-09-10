916d33eb1c41cd8d138680f60559cb6ec6d4b355
Rubik <r82072923@gmail.com>
2026-09-10 17:41:40 +0400

    Added verjinnorutyunner4 in the evocadigital page

--- src/components/evocadigital.jsx
+++ src/components/evocadigital.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import EvocaDigitaliMasin from "./evocadigital-imasin"
 import EvocaDigitaliMasin2 from "./evocadigital-imasin2"
+import VerjinNorutyunner4 from "./verjinnorutyunner4"
 function EvocaDigital(){
     return(
         <>
@@ -9,6 +10,7 @@ function EvocaDigital(){
         <TopHeader4></TopHeader4>
         <EvocaDigitaliMasin></EvocaDigitaliMasin>
         <EvocaDigitaliMasin2></EvocaDigitaliMasin2>
+        <VerjinNorutyunner4></VerjinNorutyunner4>
         </>
     )
 }
