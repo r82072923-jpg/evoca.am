90b53f9f01e8667d436dbca6e8fe59c94a8e0055
Rubik <r82072923@gmail.com>
2026-09-10 20:40:21 +0400

    Added footer2 and footerBottom in the events page

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -1,10 +1,14 @@
 import Header2 from "./header2"
 import EventsiMasin from "./events-imasin"
+import Footer2 from "./footer2"
+import FooterBottom from "./footerBottom"
 function Events(){
     return(
         <>
         <Header2></Header2>
         <EventsiMasin></EventsiMasin>
+        <Footer2></Footer2>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
