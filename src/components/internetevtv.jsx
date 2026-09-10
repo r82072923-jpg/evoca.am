38b323a59fcec02c34f423272e1b70a82e395152
Rubik <r82072923@gmail.com>
2026-09-10 23:26:59 +0400

    Added footer2 and footerBottom in the internetevtv page

--- src/components/internetevtv.jsx
+++ src/components/internetevtv.jsx
@@ -1,10 +1,14 @@
 import Header2 from "./header2"
 import InternetEvTviMasin from "./internetevtv-imasin"
+import Footer2 from "./footer2"
+import FooterBottom from "./footerBottom"
 function InternetEvTv(){
     return(
         <>
         <Header2></Header2>
         <InternetEvTviMasin></InternetEvTviMasin>
+        <Footer2></Footer2>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
