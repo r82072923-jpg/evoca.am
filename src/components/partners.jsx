e8332aecc7e23eda2cb2f262577552d1e772d247
Rubik <r82072923@gmail.com>
2026-09-10 18:12:24 +0400

    Added footer in the  parters page

--- src/components/partners.jsx
+++ src/components/partners.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader3 from "./topHeader3"
 import PartnersiMasin from "./partners-imasin"
+import Footer from "./footer"
 function Partners(){
     return(
         <>
         <Header></Header>
         <TopHeader3></TopHeader3>
         <PartnersiMasin></PartnersiMasin>
+        <Footer></Footer>
         </>
     )
 }
