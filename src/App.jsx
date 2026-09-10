8d5161ce4f3c4214886763066257ad308866ac94
Rubik <r82072923@gmail.com>
2026-09-10 23:38:36 +0400

    Added internetevtv-imasin7 in the site

--- src/App.jsx
+++ src/App.jsx
@@ -142,6 +142,7 @@ import InternetEvTviMasin3 from "./components/internetevtv-imasin3.jsx"
 import InternetEvTviMasin4 from "./components/internetevtv-imasin4.jsx"
 import InternetEvTviMasin5 from "./components/internetevtv-imasin5.jsx"
 import InternetEvTviMasin6 from "./components/internetevtv-imasin6.jsx"
+import InternetEvTviMasin7 from "./components/internetevtv-imasin7.jsx"
 function Home(){
   const [activeTab, setActiveTab] = useState('Կանխիկ');
   return(
@@ -312,6 +313,7 @@ function App() {
         <Route path="/services/internet-tv/3" element={<InternetEvTviMasin4/>}></Route>
         <Route path="/services/internet-tv/4" element={<InternetEvTviMasin5/>}></Route>
         <Route path="/services/internet-tv/5" element={<InternetEvTviMasin6/>}></Route>
+        <Route path="/services/internet-tv/6" element={<InternetEvTviMasin7/>}></Route>
       </Routes>
     </>
   )
