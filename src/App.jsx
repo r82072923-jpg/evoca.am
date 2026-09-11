17666dadb88c8209c4ebb53a2196ee598d22ec36
Rubik <r82072923@gmail.com>
2026-09-11 09:41:04 +0400

    Added komunalvjarumner-imasin8 in the site

--- src/App.jsx
+++ src/App.jsx
@@ -157,6 +157,7 @@ import KomunalVjarumneriMasin4 from "./components/komunalvjarumner-imasin4.jsx"
 import KomunalVjarumneriMasin5 from "./components/komunalvjarumner-imasin5.jsx"
 import KomunalVjarumneriMasin6 from "./components/komunalvjarumner-imasin6.jsx"
 import KomunalVjarumneriMasin7 from "./components/komunalvjarumner-imasin7.jsx"
+import KomunalVjarumneriMasin8 from "./components/komunalvjarumner-imasin8.jsx"
 function Home(){
   const [activeTab, setActiveTab] = useState('Կանխիկ');
   return(
@@ -342,6 +343,7 @@ function App() {
         <Route path="/services/utility/2" element={<KomunalVjarumneriMasin5/>}></Route>
         <Route path="/services/utility/3" element={<KomunalVjarumneriMasin6/>}></Route>
         <Route path="/services/utility/4" element={<KomunalVjarumneriMasin7/>}></Route>
+        <Route path="/services/utility/5" element={<KomunalVjarumneriMasin8/>}></Route>
       </Routes>
     </>
   )
