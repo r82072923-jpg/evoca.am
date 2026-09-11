dae6b0c842cc2387512f7c90bd6f06afd62ea6b9
Rubik <r82072923@gmail.com>
2026-09-11 13:27:47 +0400

    Added footerBottom in the loan19 page

--- src/components/loan19.jsx
+++ src/components/loan19.jsx
@@ -7,6 +7,7 @@ import Loan19iMasin3 from "./loan19-imasin3";
 import Slayder6 from "./slayder6";
 import OnlineEvMobileBanking from "./onlineevmobilebanking";
 import Footer from "./footer";
+import FooterBottom from "./footerBottom";
 function Loan19(){
     const [activeTab, setActiveTab] = useState('Վարկի մասին');
     return(
@@ -25,6 +26,7 @@ function Loan19(){
         <Slayder6></Slayder6>
         <OnlineEvMobileBanking></OnlineEvMobileBanking>
         <Footer></Footer>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
