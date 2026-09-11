be7b7c6f6f9cfb58f6caf17f6eba75137f267f6c
Rubik <r82072923@gmail.com>
2026-09-11 17:18:57 +0400

    Added footer in the hajaxordichapanishner page

--- src/components/hajaxordichapanishner.jsx
+++ src/components/hajaxordichapanishner.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import HajaxordiChapanishneriMasin from "./hajaxordichapanishner-imasin"
+import Footer from "./footer"
 function HajaxordiChapanishner(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
         <HajaxordiChapanishneriMasin></HajaxordiChapanishneriMasin>
+        <Footer></Footer>
         </>
     )
 }
