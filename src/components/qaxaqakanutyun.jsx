6ed03cf7ee2f4064f0acdae75993b9bfa2f3771c
Rubik <r82072923@gmail.com>
2026-09-11 18:13:49 +0400

    Added footer in the qaxaqakanutyun page

--- src/components/qaxaqakanutyun.jsx
+++ src/components/qaxaqakanutyun.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import QaxaqakanutyuniMasin from "./qaxaqakanutyun-imasin"
+import Footer from "./footer"
 function Qaxaqakanutyun(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
         <QaxaqakanutyuniMasin></QaxaqakanutyuniMasin>
+        <Footer></Footer>
         </>
     )
 }
