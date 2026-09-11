dccb5092f73114365a1a4ffae6bef1189efccc43
Rubik <r82072923@gmail.com>
2026-09-11 17:18:23 +0400

    Added hajaxordichapanishner-imasin in the hajaxordichapanishner page

--- src/components/hajaxordichapanishner.jsx
+++ src/components/hajaxordichapanishner.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import HajaxordiChapanishneriMasin from "./hajaxordichapanishner-imasin"
 function HajaxordiChapanishner(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
+        <HajaxordiChapanishneriMasin></HajaxordiChapanishneriMasin>
         </>
     )
 }
