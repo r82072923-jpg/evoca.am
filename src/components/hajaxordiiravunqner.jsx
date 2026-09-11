07aa0f884ff783e9a716847aab3f996f0c5ce574
Rubik <r82072923@gmail.com>
2026-09-11 16:53:46 +0400

    Added hajaxordiiravunqner-imasin in the hajaxordiiravunqner page

--- src/components/hajaxordiiravunqner.jsx
+++ src/components/hajaxordiiravunqner.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
+import HajaxordiIravunqneriMasin from "./hajaxordiiravunqner-imasin"
 function HajaxordiIravunqner(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
+        <HajaxordiIravunqneriMasin></HajaxordiIravunqneriMasin>
         </>
     )
 }
