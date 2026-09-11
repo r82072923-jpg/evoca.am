60618c74ba223dfc71b5f6774301afe6d6b5fc90
Rubik <r82072923@gmail.com>
2026-09-11 18:20:36 +0400

    Added finab=nsakanhashtarar-imasin in the finanasakahashtarar page

--- src/components/finansakanhashtarar.jsx
+++ src/components/finansakanhashtarar.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import FinansakanHashtarariMasin from "./finansakanhashtarar-imasin"
+import Footer from "./footer"
 function FinansakanHashtarar(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
         <FinansakanHashtarariMasin></FinansakanHashtarariMasin>
+        <Footer></Footer>
         </>
     )
 }
