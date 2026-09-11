b69f87ca0e9d39a50be47e2ebf6c45e157601bb6
Rubik <r82072923@gmail.com>
2026-09-11 11:11:09 +0400

    Added loan18-imasin in the loan18 page

--- src/components/loan18.jsx
+++ src/components/loan18.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader from "./topHeader"
+import Loan18iMasin from "./loan18-imasin"
 function Loan18(){
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
+        <Loan18iMasin></Loan18iMasin>
         </>
     )
 }
