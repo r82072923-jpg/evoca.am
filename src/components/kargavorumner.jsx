f9ac9e132c74cb71529c5ad12a676d8aaf48297b
Rubik <r82072923@gmail.com>
2026-09-11 17:45:48 +0400

    Added footer in the kargavorumner page

--- src/components/kargavorumner.jsx
+++ src/components/kargavorumner.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import KargavorumneriMasin from "./kargavorumner-imasin"
 import KargavorumneriMasin2 from "./kargavorumner-imasin2"
+import Footer from "./footer"
 function Kargavorumner(){
     return(
         <>
@@ -9,6 +10,7 @@ function Kargavorumner(){
         <TopHeader4></TopHeader4>
         <KargavorumneriMasin></KargavorumneriMasin>
         <KargavorumneriMasin2></KargavorumneriMasin2>
+        <Footer></Footer>
         </>
     )
 }
