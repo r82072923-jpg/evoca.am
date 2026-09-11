bea89abb18a7efbe6cf7940b0539fbeb65395a64
Rubik <r82072923@gmail.com>
2026-09-11 17:10:23 +0400

    Added footer in the hajaxordiiravunqner page

--- src/components/hajaxordiiravunqner.jsx
+++ src/components/hajaxordiiravunqner.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import HajaxordiIravunqneriMasin from "./hajaxordiiravunqner-imasin"
 import HajaxordiIravunqneriMasin2 from "./hajaxordiiravunqner-imasin2"
+import Footer from "./footer"
 function HajaxordiIravunqner(){
     return(
         <>
@@ -9,6 +10,7 @@ function HajaxordiIravunqner(){
         <TopHeader4></TopHeader4>
         <HajaxordiIravunqneriMasin> </HajaxordiIravunqneriMasin>
         <HajaxordiIravunqneriMasin2> </HajaxordiIravunqneriMasin2>
+        <Footer></Footer>
         </>
     )
 }
