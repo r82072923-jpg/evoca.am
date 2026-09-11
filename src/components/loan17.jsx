ed863eda6b5d53eee003aa033c4a3a3bd51465fb
Rubik <r82072923@gmail.com>
2026-09-11 10:06:20 +0400

    Added loan17-imasin in the loan17 page

--- src/components/loan17.jsx
+++ src/components/loan17.jsx
@@ -1,10 +1,12 @@
 import Header from "./header"
 import TopHeader from "./topHeader"
+import Loan17iMasin from "./loan17-imasin"
 function Loan17(){
     return(
         <>
         <Header></Header>
         <TopHeader></TopHeader>
+        <Loan17iMasin></Loan17iMasin>
         </>
     )
 }
