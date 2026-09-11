f1c37babebfba7f0ee1ae78318542608671e490b
Rubik <r82072923@gmail.com>
2026-09-11 18:39:55 +0400

    Added dibrary-imasin2 in the dibrary page

---
+++ src/components/dibrary-imasin2.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import DibraryiMasin from "./dibrary-imasin";
+
+function DibraryiMasin2() {
+  const highlightStyle = {
+    color: "#7e22ce",
+    fontWeight: "bold"
+  };
+
+  const textStyle = {
+    fontSize: "15px",
+    lineHeight: "1.7",
+    color: "#222",
+    fontWeight: "bold",
+    marginBottom: "24px"
+  };
+
+  return (
+    <div style={{ maxWidth: "850px", margin: "0 auto", padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <p style={textStyle}>
+        <span style={highlightStyle}>#dibrary</span>-ում կարելի է անվճար օգտվել էլեկտրոնային բիզնես ռեսուրսներից՝{" "}
+        <span style={highlightStyle}>
+          e-Duke Journals Scholarly Collection, ASTM Compass, Edward Elgar Publishing Journals, Mathematical Sciences Publishers Journals, Royal Society Journals Collection, SAGE Premier
+        </span>{" "}
+        և այլն...
+      </p>
+      <p style={textStyle}>
+        <span style={highlightStyle}>#dibrary</span>-ի գրքերը, հոդվածները և հրապարակումները շարունակաբար համալրվելու են՝ ներառելով նաև համաշխարհային լավագույն համալսարանների փակ վճարովի ռեսուրսները: Բացի էլեկտրոնային նյութերից,{" "}
+        <span style={highlightStyle}>#dibrary</span>-ում կարելի է գտնել նաև դասական գրքեր, ինչպես նաև ժամանակակից բիզնես գրականության բեսթսելերները, որոնք կարելի է կարդալ տեղում, իսկ ցանկության դեպքում՝ նաև տանել տուն:
+      </p>
+      <p style={textStyle}>
+        <span style={highlightStyle}>#dibrary</span>-ի բացման գործում մեզ աջակցել են՝{" "}
+        <span style={highlightStyle}>
+          DLAA-ը՝ Հայաստանի թվային գրադարանների ասոցիացիան (Digital Library Association of Armenia), Ավետիք Իսահակյանի անվան գրադարանը և American Corner Yerevan-ը
+        </span>
+        : Նրանց աջակցության շնորհիվ <span style={highlightStyle}>#dibrary</span>-ում կարելի է անվճար օգտվել այնպիսի հրատարակիչների նյութերից, որոնց անդամակցությունը բավականին թանկ է և շատերի համար՝ անհասանելի:
+      </p>
+    </div>
+  );
+}
+export default DibraryiMasin2
--- src/components/dibrary.jsx
+++ src/components/dibrary.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import DibraryiMasin from "./dibrary-imasin"
+import DibraryiMasin2 from "./dibrary-imasin2"
 function Dibrary(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
         <DibraryiMasin></DibraryiMasin>
+        <DibraryiMasin2></DibraryiMasin2>
         </>
     )
 }
