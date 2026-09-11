7cae716919db7b3e6ee4a874f62d515eed2e1d9b
Rubik <r82072923@gmail.com>
2026-09-11 17:45:09 +0400

    Added kargavorumner-imasin2 in the kargavorumner page

--- src/components/kargavorumner.jsx
+++ src/components/kargavorumner.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import KargavorumneriMasin from "./kargavorumner-imasin"
+import KargavorumneriMasin2 from "./kargavorumner-imasin2"
 function Kargavorumner(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
         <KargavorumneriMasin></KargavorumneriMasin>
+        <KargavorumneriMasin2></KargavorumneriMasin2>
         </>
     )
 }
