2325c3af81f5f89a7ac82ffbecb3733639acb34e
Rubik <r82072923@gmail.com>
2026-09-11 16:35:27 +0400

    Added footer in the irakanakter page

--- src/components/irakanakter.jsx
+++ src/components/irakanakter.jsx
@@ -2,6 +2,7 @@ import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import IrakanAkteriMasin from "./irakanakter-imasin"
 import IrakanAkteriMasin2 from "./irakanakter-imasin2"
+import Footer from "./footer"
 function IrakanAkter(){
     return(
         <>
@@ -9,6 +10,7 @@ function IrakanAkter(){
         <TopHeader4></TopHeader4>
         <IrakanAkteriMasin></IrakanAkteriMasin>
         <IrakanAkteriMasin2></IrakanAkteriMasin2>
+        <Footer></Footer>
         </>
     )
 }
