e30c118061fab12775a78c9662c73102c0607500
Rubik <r82072923@gmail.com>
2026-09-11 09:21:19 +0400

    Added footer2 and footerBottom in the komunalvjarumner page

--- src/components/komunalvjarumner.jsx
+++ src/components/komunalvjarumner.jsx
@@ -1,10 +1,14 @@
 import Header2 from "./header2"
 import KomunalVjarumneriMasin from "./komunalvjarumner-imasin"
+import Footer2 from "./footer2"
+import FooterBottom from "./footerBottom"
 function KomunalVjarumner(){
     return(
         <>
         <Header2></Header2>
         <KomunalVjarumneriMasin></KomunalVjarumneriMasin>
+        <Footer2></Footer2>
+        <FooterBottom></FooterBottom>
         </>
     )
 }
