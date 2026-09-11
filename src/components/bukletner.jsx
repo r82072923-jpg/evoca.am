06fbbc653124c0cdc5b33531769cb36ea39824d1
Rubik <r82072923@gmail.com>
2026-09-11 19:28:47 +0400

    Added footer in the bukletner page

--- src/components/bukletner.jsx
+++ src/components/bukletner.jsx
@@ -1,12 +1,14 @@
 import Header from "./header"
 import TopHeader4 from "./topHeader4"
 import BukletneriMasin from "./bukletner-imasin"
+import Footer from "./footer"
 function Bukletner(){
     return(
         <>
         <Header></Header>
         <TopHeader4></TopHeader4>
         <BukletneriMasin></BukletneriMasin>
+        <Footer></Footer>
         </>
     )
 }
